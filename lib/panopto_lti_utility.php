b913d96993350daf04072d90ecf38a961062a2a7
jmalmsten-panopto <jmalmsten@panopto.com>
2021-06-25 11:07:49 -0700

    fixed typo in merge conflict.

--- lib/panopto_lti_utility.php
+++ lib/panopto_lti_utility.php
@@ -44,7 +44,7 @@ class panopto_lti_utility {
                 ]
             );
 
-            if (!empty($targetservername) && strpos($type->config['toolurl'], $targetservername) !== false) {
+            if (!empty($targetservername) && strpos($type->config['toolurl'], $targetservername) !== false && 
                 $type->state == LTI_TOOL_STATE_CONFIGURED) {
                 $currentconfig = lti_get_type_config($type->id);
 
